d6073fed14ba958c6e192b543c99ef451dca8523
Robert Selleck <selleck1@lsu.edu>
2020-04-13 12:15:03 -0500

    refactor: BookShelf loop

--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -5,57 +5,34 @@ import Book from './Book'
 
 const BookShelf = props => {
   const { books, updateBookStatus } = props;
+  const eachShelf = [
+    { type: 'currentlyReading', title: 'Currently Reading' },
+    { type: 'wantToRead', title: 'Want to Read' },
+    { type: 'read', title: 'Read' }
+  ];
 
-  // surely this could be refactured...and don't call me surely
+  // go through each self and get books
   return (
     <div className="list-books-content">
-      <div>
-        <div className="bookshelf">
-          <h2 className="bookshelf-title">Currently Reading</h2>
-          <div className="bookshelf-books">
-            <ol className="books-grid">
-              {books.filter(books => books.shelf === 'currentlyReading').map(filteredBook => (
-                <Book
-                  key={filteredBook.id}
-                  book={filteredBook}
-                  books={books}
-                  updateBookStatus={updateBookStatus}
-                />
-              ))}
-            </ol>
+      {eachShelf.map((shelf, index) => {
+        return (
+          <div className="bookshelf" key={index}>
+            <h2 className="bookshelf-title">{shelf.title}</h2>
+            <div className="bookshelf-books">
+              <ol className="books-grid">
+                {books.filter(books => books.shelf === shelf.type).map(filteredBook => (
+                  <Book
+                    key={filteredBook.id}
+                    book={filteredBook}
+                    books={books}
+                    updateBookStatus={updateBookStatus}
+                  />
+                ))}
+              </ol>
+            </div>
           </div>
-        </div>
-        <div className="bookshelf">
-          <h2 className="bookshelf-title">Want to Read</h2>
-          <div className="bookshelf-books">
-            <ol className="books-grid">
-              {books.filter(books => books.shelf === 'wantToRead').map(filteredBook => (
-                <Book
-                  key={filteredBook.id}
-                  book={filteredBook}
-                  books={books}
-                  updateBookStatus={updateBookStatus}
-                />
-              ))}
-            </ol>
-          </div>
-        </div>
-        <div className="bookshelf">
-          <h2 className="bookshelf-title">Read</h2>
-          <div className="bookshelf-books">
-            <ol className="books-grid">
-              {books.filter(books => books.shelf === 'read').map(filteredBook => (
-                <Book
-                  key={filteredBook.id}
-                  book={filteredBook}
-                  books={books}
-                  updateBookStatus={updateBookStatus}
-                />
-              ))}
-            </ol>
-          </div>
-        </div>
-      </div>
+        );
+      })}
     </div>
   )
 };
